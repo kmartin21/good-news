eeb26214b642dcbd1f277bf50029f0db03a8b14d
kmartin21 <kmartin82195@gmail.com>
2019-03-11 12:28:58 -0400

    Added login test

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,20 +2,22 @@ const User = require('../models/user.model')
 const Article = require('../models/article.model')
 
 exports.loginUser = (req, res) => {
-    const user = new User({...req.body})
+    if (!req.user) res.status(500).json({ message: 'Error logging in user' })
+
+    const user = new User({...req.user})
 
     user.validate((error) => {
         if (error) res.status(400).json({message: error})
     })
 
-    let query = {}
-    let update = { expire: new Date() }
+    let query = { googleId: req.user.googleId }
+    let update = { username: req.user.username }
     let options = { upsert: true, new: true, setDefaultsOnInsert: true }
 
-    User.findOneAndUpdate(query, update, options, (error, result) => {
+    User.findOneAndUpdate(query, update, options, (error, user) => {
         if (error) res.status(500).json({ message: 'Error saving user' })
 
-        res.status(200).json({data: {user: req.user}})
+        res.status(200).json({data: {user: user}})
     })
 }
 
--- tests/routes/user.test.js
+++ tests/routes/user.test.js
@@ -4,9 +4,62 @@ const sinon = require('sinon')
 
 const User = require('../../models/user.model')
 const Article = require('../../models/article.model')
-const { saveArticle } = require('../../controllers/user.controller')
+const { saveArticle, loginUser } = require('../../controllers/user.controller')
 
 describe('User routes', () => {
+    describe('Login', () => {
+        it('should login and save a new user to the db', () => {
+
+            const expectedUser = {
+                username: 'Joe',
+                googleId: '12345',
+                savedArticles: [
+                    {
+                        source: {
+                            id: "cnn",
+                            name: "CNN"
+                        },
+                        author: "Frank Pallotta, CNN",
+                        title: "'Us' scares up screams and applause at SXSW premiere - CNN",
+                        description: "Jordan Peele is up to his old tricks. The director of \"Get Out\" was in Austin, Texas on Friday to premiere his latest horror and psychological thriller, \"Us.\"",
+                        url: "https://www.cnn.com/2019/03/09/entertainment/us-premiere/index.html",
+                        urlToImage: "https://cdn.cnn.com/cnnnext/dam/assets/181226152851-02-us-trailer-screengrab-jordan-peele-super-tease.jpg",
+                        publishedAt: "2019-03-09T15:24:00Z",
+                        content: "It should surprise no one that US is a very satisfying (and anxiety-inducing) follow up to GET OUT. What's more fascinating: that a high-concept FUNNY GAMES/Hitchcock/Cronenberg/Bradbury mashup could feel so fresh at the same time. #sxsw — erickohn (@erickohn… [+14 chars]"
+                    }
+                ]
+            }
+
+            let saveMock = sinon.mock(User)
+                           .expects('findOneAndUpdate')
+                           .yields(null, expectedUser)
+                           
+            let validateMock = sinon.mock(User.prototype)
+                               .expects('validate')
+                               .yields(null)
+
+            let req  = httpMocks.createRequest({
+                method: 'GET',
+                url: '/api/v1/auth/google/callback',
+                user: {
+                    username: 'Joe',
+                    googleId: '12345'
+                }
+            })
+
+            let res = httpMocks.createResponse()
+
+            loginUser(req, res)
+
+            const data = JSON.parse(res._getData())
+
+            assert(saveMock.calledOnce)
+            assert(validateMock.calledOnce)
+            expect(res.statusCode).to.equal(200)
+            expect(data).to.eql({ data: { user: expectedUser } })
+        })
+    })
+
     describe('Articles', () => {
         it('should save an article for a user', () => {
             let updateMock = sinon.mock(User)
